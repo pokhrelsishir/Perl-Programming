f786ae636fcfcc9176a3f8680db4a3e4612dbfc0
Sishir Pokhrel <pokhrelsishir@gmail.com>
2020-02-03 15:20:00 -0600

    Perl Hello World file's copy from puTTy !!!

---
+++ hello_world_copy.pl
@@ -0,0 +1,12 @@
+# Author: Sishir Pokhrel
+
+use strict;
+use warnings;
+
+#This is a comment in Perl
+
+
+#This line below prints Hello World !!!
+print STDOUT "Hello World !!!\n";
+
+#This is the hello_world.pl file's copy from puTTy !!! 
