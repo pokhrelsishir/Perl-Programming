7673c77c915520f1d9949233b00f420383794572
Sishir Pokhrel <pokhrelsishir@gmail.com>
2020-02-03 15:10:29 -0600

    Added fork_parent_Nchild.pl

---
+++ fork_parent_Nchild.pl
@@ -0,0 +1,34 @@
+
+#!/usr/bin/perl
+use strict;
+use warnings;
+use constant MAXCHILD => 3;
+
+my $i;    #for loop increment variable
+my $pid;
+
+print STDOUT "Parent($$): Start\n";
+
+for ($i = 0; $id < MAXCHILD; $id++)
+{
+    $pid = fork();
+    die if not defined $pid;
+
+    if ($pid == 0)
+    {
+        exec('./fork_child.pl');
+        exit;
+    }
+
+}
+
+
+for (my $id = 0; $i < MAXCHILD; $i++)
+{
+   my $child = wait();
+   print "Parent($$): Child($child) Ended\n";
+}
+
+print "Parent($$): End \n";
+
+exit;
