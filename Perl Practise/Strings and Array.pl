b4f717024a38385cff0f48724a37b4265f148c09
Sishir Pokhrel <pokhrelsishir@gmail.com>
2020-02-06 18:41:38 -0600

    Learning Perl

---
+++ Perl Practise/Strings and Array.pl
@@ -0,0 +1,27 @@
+#!/usr/bin/perl
+
+$rank=1;
+$name="Sishir";
+$totalMoney=1111111.11111;
+$my_oct=0167;          #we create oct num by starting with 0 in perl
+$my_hex=0xabe;	       #we create hexadecimal num by starting with 0x in perl
+
+print "Name: $name\n";
+print "Rank: $rank\n";
+print "Total Money Available: $totalMoney\n";
+
+print "My octal number: $my_oct\n";
+print "My Hexadecimal Number: $my_hex\n\n\n";
+
+
+print " ";
+
+@ranks = (1,2,3);             #we create arry with @
+@names = ('Bob','the','Builder');
+
+print "All ranks: @ranks\n";
+print "All names: @names\n";
+
+print "Top Player: @names[0]\n";
+
+print "Another top player: $names[2]\n";
---
+++ Perl Practise/array_operations.pl
@@ -0,0 +1,14 @@
+#!/usr/bin/perl
+
+
+@ranks = (1..10);
+@alphabets = (a..z);
+
+print "All ranks: @ranks\n";
+print "All Alphabets: @alphabets\n";
+
+$sizeAlpha=@alphabets;
+$sizeNum =@ranks;
+
+
+print "Size of Alphabets array: $sizeAlpha\nSize of Numbers array: $sizeNum\n";
